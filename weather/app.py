7bd8a498d72ea2109a2fe430e1c9ae82ed8b7370
jannebou <Janne.sainio15@gmail.com>
2024-02-05 14:33:08 +0200

    jes

--- weather/app.py
+++ weather/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import folium
+import weatherAPI
 
 app = Flask(__name__)
 
@@ -7,12 +8,12 @@ app = Flask(__name__)
 def index():
     # Create a map centered at a specific location
     map_osm = folium.Map(location=[62.791668, 22.841667], zoom_start=12)
-
+    weather = weatherAPI.get_weather("Seinäjoki")
     # Save the map to a file
     map_osm.save("templates/index.html")
 
     # Render the HTML page containing the map
-    return render_template('index.html')
+    return render_template('index.html', saa = weather)
 
 if __name__ == '__main__':
     app.run(debug=True)

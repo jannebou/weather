92862972019f6c1383e0e22701c2948077aeb191
janiM23 <jani.maki@seamk.fi>
2024-03-11 11:05:29 +0200

    kelikameroiden kuvat
    lisää kelikameroiden markerit kartalle ja esittää popupissa kuvat. huom. kuvat eivät kuitenkaan päivitä

--- weather/app.py
+++ weather/app.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template, request
-import folium
-import weatherAPI
 from flask import Flask, render_template, request, jsonify
 import folium, json, parse, requests
+import koordinaatitJaKuvat
 
 app = Flask(__name__)
 
@@ -87,40 +85,20 @@ def weatherinfo():
     print(weather_data)
     return jsonify(weather_data)
 
-if __name__ == '__main__':
-    app.run(debug=True)onclickmarkerorder = 0
+@app.route('/kelikamerat', methods=['GET'])
+def kelikamera():
+    kelikameroidenKoordinaatit = koordinaatitJaKuvat.getKoordinaatit()
+    image_urls = koordinaatitJaKuvat.getKuvat()
+    data = []
 
-app = Flask(__name__)
+    #yhdistä koordinaatit ja kuvien linkit
+    for coord, url in zip(kelikameroidenKoordinaatit, image_urls):
+        lon, lat, _ = coord
+        data.append({'lat': lat, 'lon': lon, 'image_url': url})
 
-@app.route('/')
-def index():
-    # Create a map centered at a specific location
-    map_osm = folium.Map(location=[62.791668, 22.841667], zoom_start=12)
-    weather = weatherAPI.get_weather("Seinäjoki")
-    # Save the map to a file
-    #map_osm.save("templates/index.html")
+    print(data)
+    return jsonify(data)
 
-    # Render the HTML page containing the map
-    return render_template('index.html', map=map_osm._repr_html_())
-
-@app.route('/klikkikoordinaatit', methods=['POST'])
-def map():
-    global onclickmarkerorder
-
-    if request.method == 'POST':
-        #tätä pitää kasvattaa, että tiedetään klikattujen markerien koordinaattien järjestys
-        onclickmarkerorder += 1
-
-        lat = request.form['lat']
-        lon = request.form['lon']
-        print("Koordinaatit - Latitude:", lat, "Longitude:", lon)
-
-        #tallennetaan lat ja lon sanakirjaan
-        onclickmarkerkoordinaatit[onclickmarkerorder] = {'latitude': lat, 'longitude': lon}
-        print(onclickmarkerkoordinaatit)
-        return 'Vastaanotetut koordinaatit - Latitude: {} Longitude: {}'.format(lat, lon)
-    else:
-        return 'Jotain meni vikaan.'
 
 if __name__ == '__main__':
     app.run(debug=True)
